4868e3563d3006d068c6fa8a2164c95201b5ac0d
nattawut <natauwtinsuawree@gmail.com>
2023-08-08 13:05:52 +0700

    first commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.masterauthen')
+@section('content')
+
+<div class="row justify-content-center">
+
+    <div class="col-xl-10 col-lg-12 col-md-9">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                    <div class="col-lg-6">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                            </div>
+                            <form method="POST" action="{{ route('register') }}">
+                               @csrf
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user"
+                                        id="exampleInputEmail" aria-describedby="emailHelp" name="name"
+                                        placeholder="Enter Name">
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control form-control-user"
+                                        id="exampleInputEmail" aria-describedby="emailHelp" name="email"
+                                        placeholder="Enter Email Address...">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="password" class="form-control form-control-user"
+                                        id="exampleInputPassword" placeholder="Password">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="password_confirmation" class="form-control form-control-user"
+                                        id="exampleInputPassword" placeholder="Password Confirm">
+                                </div>
+                             
+                                <input type="submit"  class="btn btn-primary btn-user btn-block" value="Rgister">
+                           
+                            </form>
+                            <hr>
+                            
+                            <div class="text-center">
+                                <a class="small" href="{{route('login')}}">Go to Login</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+</div>
+
+    
+</body>
+
+</html>
+@endsection
